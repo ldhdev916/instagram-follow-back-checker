818d9fcc09c8be4371c682916f7bd2a8bed789e7
ldhdev <dongheon0916@gmail.com>
2023-12-21 18:05:32 +0900

    Remove debugging logs from Home function
    The console logs used for debugging in the Home function were removed, as they are no longer necessary. These were primarily used to test and monitor cookie retrieval and related states like userId and confirmed. The function has been streamlined by removing these additional console outputs.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,26 +5,15 @@ import {Stack} from "@mui/material";
 import {InstagramSessionComponent} from "@/presentation/component/instagramSessionComponent";
 import {LoginWarningAlert} from "@/presentation/component/loginWarningAlert";
 import {cookies} from "next/headers";
-import {CONFIRM_COOKIE_NAME, USER_ID_COOKIE_NAME} from "@/common/consts";
+import {CONFIRM_COOKIE_NAME} from "@/common/consts";
 
 export default async function Home() {
 
-    console.log("----------------------------------")
-    console.log(typeof window)
-    console.log(cookies().getAll())
-
     const userId = getUserIdFromCookie()
-
-    console.log("userId", userId)
-    console.log("user cookie", cookies().get(USER_ID_COOKIE_NAME))
 
     const sessions = userId ? await instagramService().getSessions(userId) : []
 
     const confirmed = !!cookies().get(CONFIRM_COOKIE_NAME)
-
-    console.log("confirmed", confirmed)
-    console.log("confirmed cookie", cookies().get(CONFIRM_COOKIE_NAME))
-    console.log("----------------------------------")
 
     return <>
         {!confirmed && <LoginWarningAlert/>}
